1bd67478d30c5daac39cd7b9bf8a414f672eb008
miniguez <maig19@hotmail.com>
2015-10-09 15:29:48 -0500

    Front page HTML now generated from a template

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 
 		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)
+		self.assertIn('To-Do lists', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 		# She is invited to enter a to-do item straight away
@@ -36,7 +36,9 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows))
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
+		)
 		self.fail('Finish the test!')
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
