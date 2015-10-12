60d363185af8827c224a0620e24601a9f5d3c1c3
miniguez <maig19@hotmail.com>
2015-10-12 15:15:38 -0500

    Finishing chapter 6

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import patterns,include, url
+from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),   
+urlpatterns = patterns('',    
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),    
     # url(r'^admin/', include(admin.site.urls)),
 )
