70361398fbb5bb8561c2793df2480d709636b36a
fjimgo <fjimenezgom@gmail.com>
2015-10-16 01:43:24 -0500

    Refactor list view to handle new item POSTs

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',    
-    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+urlpatterns = [    
     url(r'^new$', 'lists.views.new_list', name='new_list'),    
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
     # url(r'^admin/', include(admin.site.urls)),
-)
+]
